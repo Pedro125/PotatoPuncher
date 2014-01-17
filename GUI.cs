474e3a995eeca71ab495f095c412b57a5fea13b5
Pedro <pedro125125@gmail.com>
2014-01-17 11:36:53 -0600

    Create GUI.cs

---
+++ GUI.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class GUI : MonoBehaviour
+{
+  //Variables
+  
+  //Screen.height
+  //Screen.width
+  
+  void OnGui()
+  {
+    GUI.Box(new Rect(0,Screen.height-100,))//WIP
+  }
+}
