0902731ebdf55a5c6c6287c662ccf54bd1035fa5
Pedro <pedro125125@gmail.com>
2014-01-17 11:57:38 -0600

    Adds three bottom buttons for upgrades

--- GUI.cs
+++ GUI.cs
@@ -15,7 +15,17 @@ public class GUI : MonoBehaviour
   {
     if (GUI.Box(new Rect(0,top,width,height),"Button One"))
     {
-      
+      Debug.Log("Button One Pressed.");
+    }
+    
+    if(GUI.Box(new Rect(width,top,width,height),"Button Two"))
+    {
+      Debug.Log("Button Two Pressed.");
+    }
+    
+    if(GUI.Box(new Rect(2*width,top,width,height),"Button Two"))
+    {
+      Debug.Log("Button Three Pressed.");
     }
   }
 }
