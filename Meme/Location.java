a2e1cec7c51426247d8bd42bb09d339711894517
Pedro <pedro125125@gmail.com>
2014-01-28 11:46:09 -0600

    Create Location.java
    Class for Doge class

---
+++ Meme/Location.java
@@ -0,0 +1,33 @@
+package meme;
+
+public class Location
+{
+    private int xPos;
+    private int yPos;
+    
+    public Location(int xPos, int yPos)
+    {
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+    
+    public int getX()
+    {
+        return xPos;
+    }
+    
+    public int getY()
+    {
+        return yPos;
+    }
+    
+    public void setX(int xPos)
+    {
+        this.xPos = xPos;
+    }
+    
+    public void setY(int yPos)
+    {
+        this.yPos = yPos;
+    }
+}
