60328dd5162a8252ea7860cca6095670454173ad
Graham Smith <gman98@me.com>
2014-01-29 15:16:26 -0600

    Javadoc support for ECBPenguin.java and SocialPenguin.java

--- Meme/ECBPenguin.java
+++ Meme/ECBPenguin.java
@@ -1,20 +1,52 @@
+/**
+ * A <code>ECBPenguin</code> represents an image or instance of the Socially Awkward/Awesome meme.
+ */
+
 public class ECBPenguin extends Meme {
   
   private String urlToUnencryptedPenguin;
   private String saveUrl;
   
+  /**
+   * Constructs an instance of <code>ECBPenguin</code>.
+   * 
+   * @param urlToUnencryptedPenguin A String value for the directory at which the original, unencrypted penguin is stored
+   * @param saveUrl A String value for the directory to which the new, encrypted penguin will be saved
+   */
   public ECBPenguin(String urlToUnencryptedPenguin, String saveUrl) { 
     super("ECBPenguin", "N/A", 0.1, "N/A", "Image")
     this.urlToUnencryptedPenguin = urlToUnencryptedPenguin;
     this.saveLocation = saveLocation;
   }
   
-  public String toCBCPenguin() {
+  /**
+   *@return saveUrl
+   */
+  public String getSaveUrl() {
+    return saveUrl;
+  }
+  
+  /**
+   * Sets a String value for the directory to which the new, encrypted penguin will be saved. Saves the penguin to the newly specified directory.
+   *@params saveUrl
+   */
+  public void setSaveUrl(String saveUrl) {
+    this.saveUrl = saveUrl;
+    //save(saveUrl);
+  }
+  
+  /**
+   * Converts the ECB penguin to a securly encrypted CBC penguin.
+   */
+  public void toCBCPenguin() {
     System.out.println("Decrypting ECBPenguin to ordinary penguin...");
     System.out.println("Encrypting ordinary penguin to CBCPenguin...");
     System.out.println("Done.");
   }
   
+  /**
+   *@return Returns a String representation of <code>ECBPenguin</code>.
+   */
   public String toString() {
     return super.toString() + "Made with " + encryptionMode + " from a file at " + urlToUnencryptedPenguin + " that was saved at " + saveLocation;
   }
--- Meme/SocialPenguin.java
+++ Meme/SocialPenguin.java
@@ -1,9 +1,20 @@
+/**
+ * A <code>SocialPenguin</code> represents an image or instance of the Socially Awkward/Awesome meme.
+ */
+
 public class SocialPenguin extends Meme {
   
   private boolean isAwkward;
   private String topText;
   private String bottomText;
   
+  /**
+   * Constructs an instance of <code>SocialPenguin</code>.
+   * 
+   * @param isAwkward A boolean value indicating whether a particular instance is awkward or awesome.
+   * @param topText The String value to be rendered at the top of the meme.
+   * @param bottomText The String value to be rendered at the bottom of the meme.
+   */
   public SocialPenguin(boolean isAwkward, String topText, String bottomText) { 
     super("Awkward Penguin", "http://knowyourmeme.com/memes/socially-awkward-penguin", 0.1, "Impact", "Image");
     if (!isAwkward) {
@@ -14,31 +25,53 @@ public class SocialPenguin extends Meme {
     this.bottomText = bottomText;
   }
   
-  
+  /**
+   *@return isAwkward
+   */
   public boolean getIsAwkward() {
     return isAwkward;
   }
   
+  /**
+   *@return Returns a boolean indicating whether a particular instance is awkward or awesome.
+   */
   public void setIsAwkward(boolean isAwkward) {
     this.isAwkward = isAwkward;
   }
   
+  /**
+   *@return Returns a boolean indicating whether a particular instance is awkward or awesome.
+   */
   public String getTopText() {
     return topText;
   }
   
+  /**
+   * Sets a String value to be rendered at the top of the meme.
+   *@params topText
+   */
   public void setTopText(String topText) {
     this.topText = topText;
   }
   
+  /**
+   *@return Returns a String value to be rendered at the bottom of the meme.
+   */
   public String getBottomText() {
     return topText;
   }
   
+  /**
+   * Sets a String value to be rendered at the bottom of the meme.
+   *@params bottomText
+   */
   public void setBottomText(String bottomText) {
     this.bottomText = bottomText;
   }
   
+  /**
+   *@return Returns a String representation of <code>SocialPenguin</code>.
+   */
   public String toString() {
     return super.toString() + "because I " + topText + " " + bottomText;
   }
